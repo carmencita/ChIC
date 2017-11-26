3d303cdb108a43cd0e962e24438e360ffebaffe2
clivi <clivi@p-clivi.local>
2017-11-26 14:51:16 +0100

    add directory

--- R/crossCorrelationInput.R
+++ R/crossCorrelationInput.R
@@ -6,117 +6,117 @@ library("girafe")
 library(caTools)
 cluster=NULL
 
-
-f_plot <- function(datax_y, chipname, plotname="plot",xlabname="x-axis",ylabname="y-axis",line=NULL, lineplotX=NULL,lineplotY=NULL) 
-{
-	#options(bitmapType='cairo')
-	
-	filename=file.path(plotsdir, paste(plotname, chipname, "pdf", sep="."))
-	print(filename)
-	pdf(filename)	
-	#print(file.path(outputdir, paste(plotname, chipname, "pdf", sep=".")))
-	#bitmap(filename,"png16m")
-	#pdf(filename=file.path(outputdir, paste(plotname, chipname, "pdf", sep=".")))
-	par(mar = c(3.5,3.5,1.0,0.5), mgp = c(2,0.65,0), cex = 0.8)
-	plot(datax_y,type='l',xlab=xlabname,ylab=ylabname)
-	abline(v=line,lty=2,lwd=2, col="red")
-	title(chipname)
-	!is.null(lineplotX)
-	{
-		lines(x=lineplotX, y=lineplotY, lwd=2, col="blue")
-	}
-	
-	dev.off()
-	return(TRUE)
-}
-
-
-f_convertFormatBroadPeak <- function(given.clusters)
-{
-	chrl <- names(given.clusters)
-	names(chrl) <- chrl
-	chrl <- chrl[unlist(lapply(given.clusters, function(d) length(d$s))) > 0]
-	md <- do.call(rbind, lapply(chrl, function(chr)
-	{ 
-		df <- given.clusters[[chr]]
-		cbind(chr, df$s, df$e, ".", "0", ".", df$rv, -1, -1)
-    	}
-	))
-    	md <- md[order(as.numeric(md[, 7]), decreasing = T), ]
-	md=data.frame(md)
-	return(md)
-}
-
-
-f_converNarrowPeakFormat =function(bd, margin = bd$whs) 
-{
-	if (is.null(margin)) {
-		margin <- 50
-	}
-	chrl <- names(bd$npl)
-	names(chrl) <- chrl
-	md <- do.call(rbind, lapply(chrl, function(chr) 
-	{
-		df <- bd$npl[[chr]]
-		x <- df$x
-		rs <- df$rs
-		if (is.null(rs)) {rs <- rep(NA, length(x))}
-	        re <- df$re
-        	if (is.null(re)) {re <- rep(NA, length(x))}
-		ivi <- which(is.na(rs))
-        	if (any(ivi)) {rs[ivi] <- x[ivi] - margin}
-		ivi <- which(is.na(re))
-        	if (any(ivi)) {re[ivi] <- x[ivi] + margin}
-		cbind(chr, rs, re, ".", "0", ".", df$y, -1, format(df$fdr, scientific = T, digits = 3), x - rs)
-	}))
-	md <- md[order(as.numeric(md[, 7]), decreasing = T), ]
-	md=data.frame(md)
-    #write.table(md, file = fname, col.names = F, row.names = F, quote = F, sep = "\t", append = F)
-	return(md)
-}
-
-
-
-
-f_getCustomStrandShift= function(x,y){
-	x=binding.characteristics$cross.correlation$x
-	y=binding.characteristics_cross.correlation_y_smoothed
-	deriv=diff(y)/diff(x)
-	#globalminY=min(abs(deriv))
-	deriv=append(0,deriv) ##to catch up the right index, because in deriv I loose one array-field
-	#globalminX=x[which(abs(deriv)==globalminY)] ###shift all um 5 nach rechts
-
-	#check from the right side and pick the points with the x (closest to zero) and y largest 
-	##means: search for regions with Vorzeichen change
-	startVorzeichen=sign(deriv[length(deriv)])
-	field=NULL
-	for (index in rev(seq(2,length(deriv))))
-	{
-		derivpoint=deriv[index]
-		xpoint=x[index]
-		ypoint=y[index]
-		#print(paste(xpoint,ypoint,sep=" "))
-		if (startVorzeichen!=sign(derivpoint))
-		{ 
-			##Vorzeichenwechsel
-			field=rbind(field,c(xpoint,ypoint,derivpoint))
-			startVorzeichen=sign(derivpoint)
-		}
-	}
-	if (is.null(field) )
-	{
-		newShift="ERROR"
-	}else{
-	
-		field=data.frame(field)
-		colnames(field)=c("x","y","deriv")
-		newShift=field[which(max(field$y)==field$y),]$x
-	}
-	return(newShift)
-}
-
 crossCorrelationInput=function(inputname="Input",read_length=36,path=getwd(),reads.aligner.type="bam")
 {
+
+	f_plot <- function(datax_y, chipname, plotname="plot",xlabname="x-axis",ylabname="y-axis",line=NULL, lineplotX=NULL,lineplotY=NULL) 
+	{
+		#options(bitmapType='cairo')
+		
+		filename=file.path(plotsdir, paste(plotname, chipname, "pdf", sep="."))
+		print(filename)
+		pdf(filename)	
+		#print(file.path(outputdir, paste(plotname, chipname, "pdf", sep=".")))
+		#bitmap(filename,"png16m")
+		#pdf(filename=file.path(outputdir, paste(plotname, chipname, "pdf", sep=".")))
+		par(mar = c(3.5,3.5,1.0,0.5), mgp = c(2,0.65,0), cex = 0.8)
+		plot(datax_y,type='l',xlab=xlabname,ylab=ylabname)
+		abline(v=line,lty=2,lwd=2, col="red")
+		title(chipname)
+		!is.null(lineplotX)
+		{
+			lines(x=lineplotX, y=lineplotY, lwd=2, col="blue")
+		}
+		
+		dev.off()
+		return(TRUE)
+	}
+
+
+	f_convertFormatBroadPeak <- function(given.clusters)
+	{
+		chrl <- names(given.clusters)
+		names(chrl) <- chrl
+		chrl <- chrl[unlist(lapply(given.clusters, function(d) length(d$s))) > 0]
+		md <- do.call(rbind, lapply(chrl, function(chr)
+		{ 
+			df <- given.clusters[[chr]]
+			cbind(chr, df$s, df$e, ".", "0", ".", df$rv, -1, -1)
+	    	}
+		))
+	    	md <- md[order(as.numeric(md[, 7]), decreasing = T), ]
+		md=data.frame(md)
+		return(md)
+	}
+
+
+	f_converNarrowPeakFormat =function(bd, margin = bd$whs) 
+	{
+		if (is.null(margin)) {
+			margin <- 50
+		}
+		chrl <- names(bd$npl)
+		names(chrl) <- chrl
+		md <- do.call(rbind, lapply(chrl, function(chr) 
+		{
+			df <- bd$npl[[chr]]
+			x <- df$x
+			rs <- df$rs
+			if (is.null(rs)) {rs <- rep(NA, length(x))}
+		        re <- df$re
+	        	if (is.null(re)) {re <- rep(NA, length(x))}
+			ivi <- which(is.na(rs))
+	        	if (any(ivi)) {rs[ivi] <- x[ivi] - margin}
+			ivi <- which(is.na(re))
+	        	if (any(ivi)) {re[ivi] <- x[ivi] + margin}
+			cbind(chr, rs, re, ".", "0", ".", df$y, -1, format(df$fdr, scientific = T, digits = 3), x - rs)
+		}))
+		md <- md[order(as.numeric(md[, 7]), decreasing = T), ]
+		md=data.frame(md)
+	    #write.table(md, file = fname, col.names = F, row.names = F, quote = F, sep = "\t", append = F)
+		return(md)
+	}
+
+
+
+
+	f_getCustomStrandShift= function(x,y){
+		x=binding.characteristics$cross.correlation$x
+		y=binding.characteristics_cross.correlation_y_smoothed
+		deriv=diff(y)/diff(x)
+		#globalminY=min(abs(deriv))
+		deriv=append(0,deriv) ##to catch up the right index, because in deriv I loose one array-field
+		#globalminX=x[which(abs(deriv)==globalminY)] ###shift all um 5 nach rechts
+
+		#check from the right side and pick the points with the x (closest to zero) and y largest 
+		##means: search for regions with Vorzeichen change
+		startVorzeichen=sign(deriv[length(deriv)])
+		field=NULL
+		for (index in rev(seq(2,length(deriv))))
+		{
+			derivpoint=deriv[index]
+			xpoint=x[index]
+			ypoint=y[index]
+			#print(paste(xpoint,ypoint,sep=" "))
+			if (startVorzeichen!=sign(derivpoint))
+			{ 
+				##Vorzeichenwechsel
+				field=rbind(field,c(xpoint,ypoint,derivpoint))
+				startVorzeichen=sign(derivpoint)
+			}
+		}
+		if (is.null(field) )
+		{
+			newShift="ERROR"
+		}else{
+		
+			field=data.frame(field)
+			colnames(field)=c("x","y","deriv")
+			newShift=field[which(max(field$y)==field$y),]$x
+		}
+		return(newShift)
+	}
+
 	source(paste(path,"GlobalParameters.R",sep=""))
 	sampleinfo_file<-paste(path,"controllist.txt",sep="")
 	sampleinfo<-read.table(sampleinfo_file,  header=TRUE, quote="", stringsAsFactors=FALSE)
